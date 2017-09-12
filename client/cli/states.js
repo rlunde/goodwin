a553c98f2403c436d9e939154a0c11ef9b678d4d
Ron Lunde <ron.lunde@iovation.com>
2017-09-11 21:38:22 -0700

    more state stuff

--- client/cli/states.js
+++ client/cli/states.js
@@ -2,40 +2,34 @@
  * Keep track of what state we're in, what to display before
  * a prompt, what to prompt, what commands to accept.
  */
-const STATE_START = 1,
-  STATE_GET_SPECIFIC_PARAMS = 2,
-  STATE_CONFIRM = 3,
-  STATE_LOAD_SESSION = 4,
-  STATE_SAVE_SESSION = 5,
-  STATE_GET_GENERAL_PARAMS = 6;
+const STATE_GET_CMD = 1,
+  STATE_GET_PARAMS = 2,
+  STATE_CONFIRM = 3;
+const cmdHelp = `At any time you can type:
+quit - to exit
+help (or just ?) - to get some help
+back - to go back a step
+gen <name> - generate a D3 sample of type <type> (e.g. gen Bar)
+config - choose where to save sample code (etc.)
+list - show the types of D3 visualizations I can generate
+save - to save the current session for later
+load - to load a previously saved session`
 
-/*
- * TODO: put in an action fn that collects input for each state that takes
- * the previous state as an arg (to go back). New Rule: don't try to make
- * something data driven until I have at least 3 instances of it!
- */
 const states = [{
-    id: STATE_START,
+    id: STATE_GET_CMD,
     name: "Start",
-    prompt: `Hello! I'm Goodwin. I'll help you generate D3 sample code.\nAt any time you can type:
-    quit - to exit
-    help (or just ?) - to get some help
-    back - to go back a step
-    gen <name> - generate a D3 sample of type <type> (e.g. gen Bar)
-    config - choose where to save sample code (etc.)
-    list - show the types of D3 visualizations I can generate
-    save - to save the current session for later
-    load - to load a previously saved session`,
-    genfn: null
+    firstprompt: `Hello! I'm Goodwin. I'll help you generate D3 sample code.\n` + cmdHelp,
+    prompt: `Enter a command (help or just ? for the list)`
   },
   {
-    id: STATE_GET_SPECIFIC_PARAMS,
+    id: STATE_GET_PARAMS,
     name: "Get visualization Params",
-    prompt: `Let's collect the parameters we need for this visualization!\nAt any time you can type:
-    quit - to exit
-    help (or just ?) - to get some help
-    back - to go back a step`
-    genfn: null
+    prompt: `Let's collect the parameters we need for this visualization`
+  },
+  {
+    id: STATE_CONFIRM,
+    name: "Confirm",
+    prompt: "(Y/n)?"
   }
 ];
 module.exports.states = states;
