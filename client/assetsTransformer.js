cb1a6b52b4d620ee1961b020648bb20c0cf6dcaa
Ron Lunde <rlunde@gmail.com>
2018-06-29 14:23:58 -0400

    fooey

--- client/assetsTransformer.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-//used by jest -- see moduleNmaeMapper in package.json
-module.exports = {
-  process(src, filename, config, options) {
-    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
-  },
-};
